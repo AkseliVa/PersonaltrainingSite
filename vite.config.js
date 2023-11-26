79ae08e844eb65d4976317eda3f106dcf394ce4b
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2023-11-26 10:59:59 +0200

    gave up on publishing to github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PersonaltrainingSite/', // Add this row and use your own repository name
   plugins: [react()]
   })
   
