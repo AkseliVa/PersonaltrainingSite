da94dc5d0e76f50fd7de1baa726443e9d9e883e1
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2023-11-26 10:53:43 +0200

    changed configurations

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/PersonaltrainingSite/', // Add this row and use your own repository name
-  plugins: [react()],
-  test: {
-  globals: true,
-  environment: 'jsdom',
-  },
+  plugins: [react()]
   })
   
   
