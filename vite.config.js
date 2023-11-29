28e2cdfecfd61bb8708a8b94ddf09d41646a1216
unknown <vartiainen.akseli736@gmail.com>
2023-11-29 16:20:57 +0200

    configs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: '/PersonaltrainingSite/', // Add this row and use your own repository name
+  plugins: [react()],
+  test: {
+  globals: true,
+  environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./node_modules.mui"),
+    }
+  }
   })
-  
-  
