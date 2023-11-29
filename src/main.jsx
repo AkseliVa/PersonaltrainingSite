c869ef02a7d37f63eeea1cebd842b4afeca872d3
unknown <vartiainen.akseli736@gmail.com>
2023-11-29 18:57:27 +0200

    test

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import './index.css'
+import './index.css';
 
 import App from './App.jsx'
 import Home from './components/Home.jsx'
