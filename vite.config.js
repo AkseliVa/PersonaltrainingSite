533123990e087c4cf96399c9d025e41aa76724f6
unknown <vartiainen.akseli736@gmail.com>
2023-11-29 16:50:05 +0200

    more configs

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PersonaltrainingSite/', // Add this row and use your own repository name
+  base: '/PersonaltrainingSite/', // Adjust this to your repository name
   plugins: [react()],
-  test: {
-  globals: true,
-  environment: 'jsdom',
+  build: {
+    assetsDir: 'assets',
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./node_modules.mui"),
-    }
-  }
-  })
+      "@mui": path.resolve(__dirname, "./node_modules/@mui"),
+    },
+  },
+});
