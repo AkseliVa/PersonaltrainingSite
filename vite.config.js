de82850fe4eb1dcebc7d79a17d78c21e71512717
unknown <vartiainen.akseli736@gmail.com>
2023-11-29 17:56:03 +0200

    trying to publish on gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
-  base: '/PersonaltrainingSite/', // Adjust this to your repository name
+  base: '/PersonaltrainingSite/', // Add this row and use your own repository name
   plugins: [react()],
-  build: {
-    assetsDir: 'dist/assets',
+  test: {
+  globals: true,
+  environment: 'jsdom',
   },
-  resolve: {
-    alias: {
-      "@mui": path.resolve(__dirname, "./node_modules/@mui"),
-    },
-  },
-});
+  })
