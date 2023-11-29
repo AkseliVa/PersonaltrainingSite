a6a81240f5def077b298f4fb1e29fafeba502b05
unknown <vartiainen.akseli736@gmail.com>
2023-11-29 19:06:40 +0200

    render

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/PersonaltrainingSite/', // Add this row and use your own repository name
   plugins: [react()],
-  test: {
-  globals: true,
-  environment: 'jsdom',
-  },
-  })
+})
